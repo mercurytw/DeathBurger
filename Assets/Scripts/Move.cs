20db074be7c499f7099ac566126d29c3d10931f4
Alex Vaughn <akjlsgbfklajdgbfibnfbfg>
2016-01-30 03:13:35 -0800

    fixed player rotation

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,15 +6,20 @@ public class Move : MonoBehaviour
 {
     public const float upSpeed = 5.0f;
     public const float sideSpeed = 5.0f;
+    private GameObject model;
+    private Camera cam;
 
     private Rigidbody phys_obj;
     private Vector3 move_velocity = new Vector3();
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         phys_obj = GetComponent<Rigidbody>();
         Debug.Assert(phys_obj);
-	}
+        model = gameObject.transform.GetChild(2).gameObject;
+        cam = Camera.main;
+    }
 
 	// Update is called once per frame
 	void Update ()
@@ -23,10 +28,29 @@ public class Move : MonoBehaviour
         float side = Input.GetAxis("Horizontal") * sideSpeed;
         move_velocity.Set(side, 0.0f, up);
         phys_obj.velocity = move_velocity;
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(ray);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.tag=="Finish")
+            {
+                model.transform.LookAt(new Vector3(hit.point.x,this.transform.position.y,hit.point.z));
+                break;
+            }
+        }
+
+
+
+        //transform.Rotate(new Vector3(/*Input.GetAxis("Mouse Y")*/0,Input.GetAxis("Mouse X") * Time.deltaTime * turnSpeed,0));
+        //gameObject.transform.GetChild(2).Rotate(new Vector3(/*Input.GetAxis("Mouse Y")*/0, Input.GetAxis("Mouse X") * Time.deltaTime * turnSpeed, 0));
     }
 
-    void OnCollisionEnter(Collision col) {
-        if (col.gameObject.tag == "Enemy") {
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
