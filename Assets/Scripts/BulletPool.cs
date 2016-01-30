1d964afffa29e5d712872f6b71da7db57f354ddc
AnhQuan Nguyen <j4qfrost@gmail.com>
2016-01-30 01:18:50 -0800

    bullet interface stuff

---
+++ Assets/Scripts/BulletPool.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BulletPool : MonoBehaviour {
+
+	public int amount = 100;
+	public string type = "Pickles";
+	List<IBullet> pool = new List<IBullet>();
+
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+
+	public void GetBullet(float x, float z, Quaternion heading) {
+		if (amount < 1)
+			return;
+
+		IBullet newBullet = new IBullet ();
+		amount--;
+		pool.Add(newBullet.initialize (x, z, heading, this));
+	}
+
+	public void ReleaseBullet(IBullet bullet) {
+		pool.Remove (bullet);
+		amount++;
+	}
+}
---
+++ Assets/Scripts/IBullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IBullet : MonoBehaviour {
+
+	public int damage = 1;
+	public string type = "Pickles";
+	BulletPool bPool;
+
+	float x;
+	float z;
+	Quaternion heading;
+
+	// Use this for initialization
+	public IBullet initialize(float nx, float nz, Quaternion nheading, BulletPool pool) {
+		x = nx;
+		z = nz;
+		heading = nheading;
+		bPool = pool;
+		return this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDestroy() {
+		bPool.ReleaseBullet (this);
+	}
+
+	void OnHit() {
+		//Special Effects
+		Destroy (this);
+	}
+}
