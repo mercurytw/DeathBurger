932fcb87eed4e3d468ffb87489e9d3c391868d9a
Alex Vaughn <akjlsgbfklajdgbfibnfbfg>
2016-01-30 06:07:36 -0800

    Move unfucked

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -12,15 +12,23 @@ public class Move : MonoBehaviour
     private Rigidbody phys_obj;
     private Vector3 temp_vec = new Vector3();
 
+    public BulletPool gun;
+    public const float delay = 0.2f;
+    public float timer = 0.0f;
+
     // Use this for initialization
     void Start ()
     {
         phys_obj = GetComponent<Rigidbody>();
         Debug.Assert(phys_obj);
         model = gameObject.transform.GetChild(2).gameObject;
+        Debug.Assert(model);
         cam = Camera.main;
+
+        gun = new BulletPool(20, "Bullet");
     }
 
+    private bool bShoot = true;
 	// Update is called once per frame
 	void Update ()
     {
@@ -30,15 +38,24 @@ public class Move : MonoBehaviour
         phys_obj.velocity = temp_vec;
 
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit[] hits = Physics.RaycastAll(ray);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(ray);
         foreach (RaycastHit hit in hits)
         {
-            if (hit.collider.tag=="Floor")
+            if (hit.collider.tag=="Finish")
             {
-                temp_vec.Set(hit.point.x, model.transform.position.y, hit.point.z);
-                model.transform.LookAt(temp_vec);
+                Vector3 pointVector = new Vector3(hit.point.x, model.transform.position.y, hit.point.z);
+                model.transform.LookAt(pointVector);
+                //if (timer <= Time.time)
                 break;
             }
+        }
+
+        if (bShoot)
+        {
+            bShoot = false;
+            gun.GetBullet(transform.position.x, transform.position.z, model.transform.rotation);
+            timer = Time.time + delay;
         }
     }
 
