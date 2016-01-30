ed93ef9317a3743483138919e3c49d13b1a69319
Alex Vaughn <akjlsgbfklajdgbfibnfbfg>
2016-01-29 23:41:34 -0800

    first commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    public const float upSpeed = 5.0f;
+    public const float sideSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (Input.GetKey(KeyCode.W))
+        //{
+
+        //}
+
+        float up = Input.GetAxis("Vertical") * upSpeed * Time.deltaTime;
+        float side = Input.GetAxis("Horizontal") * sideSpeed * Time.deltaTime;
+
+        transform.Translate(side,0,up);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+}
