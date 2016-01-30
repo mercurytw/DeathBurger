1b1e9e9e4d69ad5f5a43057a01defc14322d7896
AnhQuan Nguyen <j4qfrost@gmail.com>
2016-01-30 10:30:44 -0800

    wave and wave controllers

---
+++ Assets/Scripts/IEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IEnemy : MonoBehaviour {
+
+	public string type;
+	public float x;
+	public float z;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnemy (string ntype, float nx, float nz) {
+		type = ntype;
+		x = nx;
+		z = nz;
+	}
+}
