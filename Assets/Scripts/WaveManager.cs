4aa6a29bce8d9b5533fb1cffc6b35439c4c93301
Jeffrey Cochran <mercurythewhite@gmail.com>
2016-01-30 18:55:09 -0800

    ALMOST MVP!!! HYYYYYYPE

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,21 +4,35 @@ using System.Collections;
 
 public class WaveManager : MonoBehaviour {
     private int num_of_enemies = 0;
+    private string[] waves = {"wave1", "wave2"};
+    private int curr_wave_idx = 0;
+
     void Start() {
-        num_of_enemies = GameObject.FindGameObjectsWithTag("Respawn").Length;
+        seeetup();
+    }
+
+    void seeetup() {
+        num_of_enemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
         EventManager.OnDeath += OnDeath;
+        
     }
 
     void OnDestroy() {
         EventManager.OnDeath -= OnDeath;
     }
 
+    private int level_to_load = -1;
+    void FixedUpdate() {
+        if (-1 != level_to_load)
+            SceneManager.LoadScene(level_to_load);
+    }
+
     void OnDeath(Death death) {
         if ("Enemy" != death.victim_obj.tag)
             return;
-
-        if (0 == --num_of_enemies) {
-            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount);
+       // Debug.Log("Ded");
+        if (0 <= --num_of_enemies) {
+            level_to_load = (int)((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
         }
     }
 #if BUTTS
