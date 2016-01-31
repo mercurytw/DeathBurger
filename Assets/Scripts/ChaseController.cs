09077a18f96fe52bb5edb76fbb1229af02a782a9
Jeffrey Cochran <mercurythewhite@gmail.com>
2016-01-31 11:29:30 -0800

    Navmesh and navmesh agents

--- Assets/Scripts/ChaseController.cs
+++ Assets/Scripts/ChaseController.cs
@@ -6,6 +6,8 @@ public class ChaseController : MonoBehaviour {
     private Rigidbody body;
     private GameObject player;
     private Vector3 move_vector = new Vector3();
+    private NavMeshAgent nav_agent = null;
+    private bool use_naieve_pathing = true;
 
 	// Use this for initialization
 	void Start () {
@@ -13,17 +15,30 @@ public class ChaseController : MonoBehaviour {
         Debug.Assert(body);
         player = GameObject.FindWithTag("Player");
         Debug.Assert(player);
+        nav_agent = GetComponent<NavMeshAgent>();
+        use_naieve_pathing = null == nav_agent;
 	}
-    float time = 0;
+    
 	// Update is called once per frame
 	void Update () {
         move_vector.Set(player.transform.position.x, 
                         transform.position.y, // intentionally OUR transform, NOT player transform!
                         player.transform.position.z);
         transform.LookAt(move_vector);
+        if (!use_naieve_pathing)
+            return;
+
         move_vector.Set(transform.forward.x * move_speed, 0.0f, transform.forward.z * move_speed);
-        if (0f == (float)(Time.realtimeSinceStartup % 10))
-            Debug.Log(move_vector);
         body.velocity = move_vector;
 	}
+
+    // non-naieve navigation is expensive, so only run it on fixed update
+    void FixedUpdate() {
+        if (use_naieve_pathing)
+            return;
+        move_vector.Set(player.transform.position.x,
+                        transform.position.y, // intentionally OUR transform, NOT player transform!
+                        player.transform.position.z);
+        nav_agent.SetDestination(move_vector);
+    }
 }
---
+++ Assets/Scripts/ChaseNavMesh.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseNavMesh : MonoBehaviour {
+    private NavMeshAgent agent = null;
+    private GameObject player = null;
+    private Vector3 target = new Vector3();
+
+    // move me out into a header or some shit
+    public enum LogicType
+    {
+        bunny,
+        penguin
+    }
+    public LogicType logic;
+
+    // hacky bulllshit
+    // code duplication with ChaseNavMesh.cs
+    private float getHeightForTarget() {
+        if (LogicType.bunny == logic) {
+            return 0.321f;
+        }
+        return 3f;
+    }
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        Debug.Assert(agent);
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Assert(player);
+	}
+	
+	// Pathing is probably expensive, so set it on fixed update
+    void FixedUpdate() {
+        target.Set(player.transform.position.x,
+                   getHeightForTarget(),
+                   player.transform.position.z);
+        agent.SetDestination(target);
+    }
+}
