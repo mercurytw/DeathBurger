039b6badb73b0420e999dc91360207dda60a0125
Mahaela <mkj002@ucsd.edu>
2016-01-31 09:57:24 -0800

    opening scene

---
+++ Assets/Scripts/Opening Scene/LightMover.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightMover : MonoBehaviour {
+
+    public Light topLight;
+    public Light bottomLight;
+    public Light spotLight1;
+    public Light spotLight2;
+    public Light spotLight3;
+    public Light spotLight4;
+    public float topLightOnTime;
+    public float bottomLightOnTime;
+    public float topBottomLightMoveDistance;
+    public float topBottomLightMoveSpeed;
+    public float spotLight1OnTime;
+    public float spotLight2OnTime;
+    public float spotLight3OnTime;
+    public float spotLight4OnTime;
+    private float time;
+    private Vector3 topLightEndDistance;
+    private Vector3 bottomLightEndDistance;
+	// Use this for initialization
+	void Start () {
+        float xPos = topLight.transform.position.x;
+        xPos -= topBottomLightMoveDistance;
+        topLightEndDistance = new Vector3(xPos, topLight.transform.position.y, topLight.transform.position.z);
+        xPos = bottomLight.transform.position.x;
+        xPos += topBottomLightMoveDistance;
+        bottomLightEndDistance = new Vector3(xPos, bottomLight.transform.position.y, bottomLight.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+
+
+        //Move Top Light
+        if(time > topLightOnTime && topLight.transform.position.x >= topLightEndDistance.x && topLight.isActiveAndEnabled)
+        {
+            topLight.transform.position = MoveLight(topLight.transform.position, topLightEndDistance);
+        }
+        if(topLight.isActiveAndEnabled && topLight.transform.position.x <= topLightEndDistance.x)
+        {
+            topLight.enabled = false;
+        }
+
+        //Move Bottom Light
+        if (time > bottomLightOnTime && bottomLight.transform.position.x <= bottomLightEndDistance.x && bottomLight.isActiveAndEnabled)
+        {
+            bottomLight.transform.position = MoveLight(bottomLight.transform.position, bottomLightEndDistance);
+        }
+        if (bottomLight.isActiveAndEnabled && bottomLight.transform.position.x >= bottomLightEndDistance.x)
+        {
+            bottomLight.enabled = false;
+        }
+        if(spotLight1OnTime< time && !spotLight1.isActiveAndEnabled)
+        {
+            spotLight1.enabled = true;
+        }
+        if(spotLight2OnTime < time && !spotLight2.isActiveAndEnabled)
+        {
+            spotLight2.enabled = true;
+        }
+        if (spotLight3OnTime < time && !spotLight3.isActiveAndEnabled)
+        {
+            spotLight3.enabled = true;
+        }
+        if(spotLight4OnTime < time && !spotLight4.isActiveAndEnabled)
+        {
+            spotLight4.enabled = true;
+        }
+    }
+
+    Vector3 MoveLight(Vector3 start, Vector3 end)
+    {
+        return (Vector3.MoveTowards(start, end, topBottomLightMoveSpeed));
+    }
+}
---
+++ Assets/Scripts/Opening Scene/TextRotator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextRotator : MonoBehaviour {
+
+    public float rotateStartTime;
+    public float turnSpeed;
+    private float time;
+    private float rotationTracker = 0;
+    private float startRotation = 0;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time > rotateStartTime && rotationTracker < 90)
+        {
+            startRotation = transform.rotation.x;
+            transform.Rotate(new Vector3(1, 0, 0) *-turnSpeed * Time.deltaTime);
+            rotationTracker += transform.position.x - startRotation;
+        }
+
+	}
+}
