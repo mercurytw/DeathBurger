55b49110b7d54ab0fe54b259add668cf0f01ac46
Jeffrey Cochran <mercurythewhite@gmail.com>
2016-02-01 19:36:04 -0800

    Fixed some ui stuff and let you skip the intro by pressing enter.

--- Assets/Scripts/OpeningSeq.cs
+++ Assets/Scripts/OpeningSeq.cs
@@ -4,29 +4,14 @@ using System.Collections;
 
 public class OpeningSeq : MonoBehaviour
 {
-    public Texture imageToDisplay;
     public float timeToDisplayImage;
     public int nextLevelToLoad;
 
-    private float timeForNextLevel;
-
-    public void Start()
-    {
-        StartCoroutine("Countdown");
+    void Update() {
+        if ((0 >= (timeToDisplayImage -= Time.deltaTime)) ||
+            (Input.GetKeyDown(KeyCode.Return))) {
+            SceneManager.LoadScene(nextLevelToLoad);
+        }
+        
     }
-
-    private IEnumerator Countdown()
-    {
-        yield return new WaitForSeconds(timeToDisplayImage);
-        SceneManager.LoadScene(nextLevelToLoad);
-    }
-
-    //public void OnGUI()
-    //{
-    //    GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), imageToDisplay);
-    //    if (Time.time >= timeForNextLevel)
-    //    {
-    //        SceneManager.LoadScene("Opening Scene");
-    //    }
-    //}
 }
